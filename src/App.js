f936ca91bcca1303bd00d88d1f4ab8249bd00f80
Jared Vandeventer <vanthedev@gmail.com>
2019-10-08 13:48:49 -0600

    Added chroma-js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from './Palette'
 import seedColors from './SeedColor'
 import './App.css';
+import { generatePalette } from './colorHelper'
 
-function App() {
-  return (
-    <div>
-      <Palette {...seedColors[5]} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]))
+    return(
+      <div className="App">
+        <Palette {...seedColors[4]} />
+      </div>
+    )
+  }
 }
 
 export default App;
