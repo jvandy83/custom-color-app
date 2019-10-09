00489e411d1ffeead5b79f36f1d8085ce4a1671b
Jared Vandeventer <vanthedev@gmail.com>
2019-10-09 10:09:37 -0600

    Added slider-bar

--- src/colorHelper.js
+++ src/colorHelper.js
@@ -44,10 +44,11 @@ function generatePalette(starterPalette) {
       newPalette.colors[levels[i]].push({
         name: `${color.name} ${levels[i]}`,
         id: color.name.toLowerCase().replace(/ /g, "-"),
+        hex: scale[i],
         rgb: chroma(scale[i]).css(),
         rgba: chroma(scale[i])
           .css()
-          .replace("rgba", "rgba")
+          .replace("rgb", "rgba")
           .replace(")", ",1.0)")
       })
     }
