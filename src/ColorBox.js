a03b668e38d0b07164fb6c9e3eaaf8dbd446a752
Jared Vandeventer <vanthedev@gmail.com>
2019-10-13 12:17:21 -0600

    Adjust color based on luminence

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import './ColorBox.css'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { Link } from 'react-router-dom'
+import chroma from 'chroma-js'
+import './ColorBox.css'
 
 class ColorBox extends Component {
   constructor(props) {
@@ -22,6 +23,8 @@ class ColorBox extends Component {
   render() {
     const {name, background, moreUrl, showLink} = this.props
     const { copied } = this.state
+    const isDarkColor = chroma(background).luminance() <= 0.08
+    const isLightColor = chroma(background).luminance() >= 0.07
     return(
       <CopyToClipboard text={background} onCopy={this.changeCopyState} >
         <div style={{ background }} className="ColorBox">
@@ -29,19 +32,19 @@ class ColorBox extends Component {
             style={{ background }} 
             className={`copy-overlay ${copied && "show"}`}
           />
-          <div className={`copy-msg ${copied && 'show'}`}>
+          <div className={`copy-msg ${copied && "show"}`}>
             <h1>copied!</h1>
-            <p>{background}</p>
+            <p className={isLightColor && 'dark-text'}>{background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span>{name}</span>
+              <span className={isDarkColor && 'light-text'}>{name}</span>
             </div>
-            <button className="copy-button">Copy</button>
+            <button className={`copy-button ${isLightColor && 'dark-text'}`}>Copy</button>
           </div>
           {showLink && (
             <Link to={moreUrl} onClick={this.handleClick}>
-              <span className="see-more">More</span>
+              <span className={`see-more ${isLightColor && "dark-text"}`}>More</span>
             </Link>    
           )}
         </div>
